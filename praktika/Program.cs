c651d8e8b838678591a948813e60c1672dad89b3
student <student@C251U26>
2020-09-04 12:02:08 +0300

    1

---
+++ praktika/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace praktika
+{
+  struct P
+  {
+    static void Main(string[] args)
+    {
+      string s = "Microsoft .NET Framework 2 Application Development Foundation";
+      string[] sa = s.Split(" ");
+      Array.Sort(sa);
+      s = string.Join(" ", sa); Console.WriteLine(s);
+    }
+  }
+}
